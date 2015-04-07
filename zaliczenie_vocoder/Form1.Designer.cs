131429368e2fd078ffd2901fb61b0ac5bbec7b7b
krzysztofchadynka <kchadynka@o2.pl>
2015-04-07 23:49:34 +0200

    Create very simple main layout ;)

--- zaliczenie_vocoder/Form1.Designer.cs
+++ zaliczenie_vocoder/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace vocoder
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.new_record_bnt = new System.Windows.Forms.Button();
+            this.bnt_open_file = new System.Windows.Forms.Button();
+            this.bnt_close_app = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.koniecToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calisto MT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amazing Voice";
+            // 
+            // new_record_bnt
+            // 
+            this.new_record_bnt.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.new_record_bnt.Location = new System.Drawing.Point(12, 114);
+            this.new_record_bnt.Name = "new_record_bnt";
+            this.new_record_bnt.Size = new System.Drawing.Size(139, 79);
+            this.new_record_bnt.TabIndex = 1;
+            this.new_record_bnt.Text = "New record";
+            this.new_record_bnt.UseVisualStyleBackColor = true;
+            // 
+            // bnt_open_file
+            // 
+            this.bnt_open_file.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bnt_open_file.Location = new System.Drawing.Point(320, 114);
+            this.bnt_open_file.Name = "bnt_open_file";
+            this.bnt_open_file.Size = new System.Drawing.Size(139, 79);
+            this.bnt_open_file.TabIndex = 2;
+            this.bnt_open_file.Text = "Open file";
+            this.bnt_open_file.UseVisualStyleBackColor = true;
+            // 
+            // bnt_close_app
+            // 
+            this.bnt_close_app.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bnt_close_app.Location = new System.Drawing.Point(169, 220);
+            this.bnt_close_app.Name = "bnt_close_app";
+            this.bnt_close_app.Size = new System.Drawing.Size(139, 79);
+            this.bnt_close_app.TabIndex = 3;
+            this.bnt_close_app.Text = "Close";
+            this.bnt_close_app.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(471, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.koniecToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // koniecToolStripMenuItem
+            // 
+            this.koniecToolStripMenuItem.Name = "koniecToolStripMenuItem";
+            this.koniecToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.koniecToolStripMenuItem.Text = "New record";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(471, 351);
+            this.Controls.Add(this.bnt_close_app);
+            this.Controls.Add(this.bnt_open_file);
+            this.Controls.Add(this.new_record_bnt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Amazing Voice";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button new_record_bnt;
+        private System.Windows.Forms.Button bnt_open_file;
+        private System.Windows.Forms.Button bnt_close_app;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem koniecToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
     }
 }
 
