a3a80955f647488ff113878838940b5da6aaa4b9
pfitzner <patrykfitzner@gmail.com>
2015-05-20 16:40:09 +0200

    Open and Play wave file

--- zaliczenie_vocoder/Form1.Designer.cs
+++ zaliczenie_vocoder/Form1.Designer.cs
@@ -44,18 +44,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calisto MT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(117, 39);
+            this.label1.Location = new System.Drawing.Point(156, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(235, 36);
+            this.label1.Size = new System.Drawing.Size(290, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Amazing Voice";
             // 
             // new_record_bnt
             // 
             this.new_record_bnt.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.new_record_bnt.Location = new System.Drawing.Point(12, 114);
+            this.new_record_bnt.Location = new System.Drawing.Point(16, 140);
+            this.new_record_bnt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.new_record_bnt.Name = "new_record_bnt";
-            this.new_record_bnt.Size = new System.Drawing.Size(139, 79);
+            this.new_record_bnt.Size = new System.Drawing.Size(185, 97);
             this.new_record_bnt.TabIndex = 1;
             this.new_record_bnt.Text = "New record";
             this.new_record_bnt.UseVisualStyleBackColor = true;
@@ -64,22 +66,26 @@
             // bnt_open_file
             // 
             this.bnt_open_file.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bnt_open_file.Location = new System.Drawing.Point(320, 114);
+            this.bnt_open_file.Location = new System.Drawing.Point(427, 140);
+            this.bnt_open_file.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bnt_open_file.Name = "bnt_open_file";
-            this.bnt_open_file.Size = new System.Drawing.Size(139, 79);
+            this.bnt_open_file.Size = new System.Drawing.Size(185, 97);
             this.bnt_open_file.TabIndex = 2;
             this.bnt_open_file.Text = "Open file";
             this.bnt_open_file.UseVisualStyleBackColor = true;
+            this.bnt_open_file.Click += new System.EventHandler(this.bnt_open_file_Click);
             // 
             // bnt_close_app
             // 
             this.bnt_close_app.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bnt_close_app.Location = new System.Drawing.Point(165, 217);
+            this.bnt_close_app.Location = new System.Drawing.Point(220, 267);
+            this.bnt_close_app.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bnt_close_app.Name = "bnt_close_app";
-            this.bnt_close_app.Size = new System.Drawing.Size(139, 79);
+            this.bnt_close_app.Size = new System.Drawing.Size(185, 97);
             this.bnt_close_app.TabIndex = 3;
             this.bnt_close_app.Text = "Close";
             this.bnt_close_app.UseVisualStyleBackColor = true;
+            this.bnt_close_app.Click += new System.EventHandler(this.bnt_close_app_Click);
             // 
             // menuStrip1
             // 
@@ -87,7 +93,8 @@
             this.plikToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(471, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(628, 28);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -98,38 +105,39 @@
             this.openToolStripMenuItem,
             this.closeToolStripMenuItem});
             this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
-            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
             this.plikToolStripMenuItem.Text = "Plik";
             // 
             // koniecToolStripMenuItem
             // 
             this.koniecToolStripMenuItem.Name = "koniecToolStripMenuItem";
-            this.koniecToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.koniecToolStripMenuItem.Size = new System.Drawing.Size(155, 24);
             this.koniecToolStripMenuItem.Text = "New record";
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(155, 24);
             this.openToolStripMenuItem.Text = "Open...";
             // 
             // closeToolStripMenuItem
             // 
             this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(155, 24);
             this.closeToolStripMenuItem.Text = "Close";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(471, 351);
+            this.ClientSize = new System.Drawing.Size(628, 432);
             this.Controls.Add(this.bnt_close_app);
             this.Controls.Add(this.bnt_open_file);
             this.Controls.Add(this.new_record_bnt);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "Amazing Voice";
             this.menuStrip1.ResumeLayout(false);
--- zaliczenie_vocoder/record.cs
+++ zaliczenie_vocoder/record.cs
@@ -5,13 +5,16 @@ using System.Text;
 using Microsoft.Xna.Framework.Audio;
 using NAudio;
 using NAudio.Wave;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace vocoder
 {
-    class Record
+    public class Record
     {
         private WaveIn source;
         private WaveFileWriter waveFile;
+        private AudioFileReader reader;
 
         public Record()
         {
@@ -29,9 +32,27 @@ namespace vocoder
         {
         }
 
-        public void openFile()
+        public void openFile(String path)
         {
-           
+            this.reader = new AudioFileReader(path);
+            play(100);
+        }
+
+        public void play(int time)
+        {
+            if (this.reader != null)
+            {
+                IWavePlayer player = new WaveOut(WaveCallbackInfo.FunctionCallback());
+                player.Init(this.reader);
+                player.Play();
+                if (time > 0) { Thread.Sleep(time); }// for pasued thread after 5sec
+                player.Stop();
+                player.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("You didn't select any audio file to play");
+            }
         }
     }
 }
